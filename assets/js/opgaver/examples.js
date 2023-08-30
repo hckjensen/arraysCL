144f04f65fb99b1e4e36076c1d87ee1b16e8a722
hckjensen <hckjensen96@gmail.com>
2023-08-30 11:59:46 +0200

    Update examples.js

--- assets/js/opgaver/examples.js
+++ assets/js/opgaver/examples.js
@@ -8,13 +8,26 @@ myExtrasInput.addEventListener("change", addExtra);
 
 
 function addExtra() {
+    myExtras.shift()
+    myExtras.push(myExtrasInput.value)
+    console.log(myExtras);
+    updateExtras();
     /* din map kode her... */
 }
 
 
 
 function updateExtras() {
+    let extraText = myExtras.map((x) =>{
+        return x;
+    })
+    let list = document.createElement("li");
+    let item = document.createTextNode(extraText);
+    list.appendChild(item); // l
+    myExtrasListElement.appendChild(list);
 
+
+    // console.log(extraText);
 
 
 
